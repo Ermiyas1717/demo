4752c52a026109770dda0f842b94a8c9504817d6
Ermiyas Alemu <ermiyasalemu586@gmail.com>
2024-01-11 22:10:49 +0300

    saved in json

--- src/main/java/emd/App.java
+++ src/main/java/emd/App.java
@@ -1,6 +1,10 @@
 package emd;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -8,8 +12,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        // JSONObject object = new JSONObject();
+        JSONObject object = new JSONObject();
+
+        JSONArray array = new JSONArray();
 
         Scanner scan = new Scanner(System.in);
 
@@ -22,10 +27,11 @@ public class App {
         tracer.toDoName = scan.nextLine();
 
         System.out.print("enter project No: ");
-        tracer.toDoNumber = scan.nextInt();
+        tracer.toDoID = scan.nextInt();
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime finishingDate = LocalDateTime.parse(tracer.toDoDate, formatter);
+        LocalDateTime finishingDate = LocalDateTime.parse(tracer.toDoDate,
+                formatter);
         LocalDateTime currentDate = LocalDateTime.now();
         Duration duration = Duration.between(currentDate, finishingDate);
 
@@ -34,10 +40,24 @@ public class App {
         long remainingMinutes = duration.toMinutes() % 60;
         long remainingSeconds = duration.getSeconds() % 60;
 
-        System.out.println("tracer to Do Number: " + tracer.toDoNumber);
+        System.out.println("tracer to Do ID: " + tracer.toDoID);
         System.out.println("tracer to Do Name: " + tracer.toDoName);
-        System.out.println("Remaining time: " + remainingDays + " days, " + remainingHours + " hours, "
-                + remainingMinutes + " minutes, " + remainingSeconds + " seconds.");
+        System.out.println("Remaining time: " + remainingDays + " days, " +
+                remainingHours + " hours, " + remainingMinutes + " minutes, " + remainingSeconds + " seconds.");
+
+        array.add(tracer.toDoName);
+        array.add(tracer.toDoDate);
+        object.put(tracer.toDoID, array);
+
+        try {
+            String path = "C:\\Users\\al\\Desktop\\projects\\maven\\demo\\src\\main\\file.json";
+            FileWriter file = new FileWriter(path);
+            String json = object.toJSONString();
+            file.write(json);
+            file.close();
+        } catch (IOException io) {
+            System.out.println("Error while writing to file.");
+        }
 
     }
 }
--- src/main/java/emd/Tracker.java
+++ src/main/java/emd/Tracker.java
@@ -3,10 +3,10 @@ package emd;
 public class Tracker {
 
     String toDoName, remainTime, toDoDate = "2024-02-17 12:00:00";
-    int toDoNumber = 0;
+    int toDoID = 0;
 
     void trac(int no, String name, String date, String remain) {
-        toDoNumber = no;
+        toDoID = no;
         toDoName = name;
         toDoDate = date;
         remainTime = remain;
